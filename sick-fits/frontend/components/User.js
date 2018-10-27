8e6a37428743d458d8a3fd5413dc317581e0f4c4
Bridger Putnam <putnam@Bridgers-MBP.PK5001Z>
2018-10-27 15:20:11 -0600

    Fix CURRENT_USER_QUERY 'me' is not the name of this query, we're querying the 'me' defined in Query.js

--- sick-fits/frontend/components/User.js
+++ sick-fits/frontend/components/User.js
@@ -3,11 +3,13 @@ import gql from 'graphql-tag'
 import PropTypes from 'prop-types'
 
 const CURRENT_USER_QUERY = gql`
-  query me {
-    id
-    email
-    name
-    permissions
+  query {
+    me {
+      id
+      email
+      name
+      permissions
+    }
   }
 `
 
