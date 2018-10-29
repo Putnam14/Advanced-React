e6099115c46eb665f42547b4d53f53fea3e99deb
Putnam14 <putnambr76@gmail.com>
2018-10-28 20:24:43 -0600

    Add sign in box

--- sick-fits/frontend/pages/signup.js
+++ sick-fits/frontend/pages/signup.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import Signup from '../components/Signup'
+import Signin from '../components/Signin'
 
 const Columns = styled.div`
   display: grid;
@@ -10,8 +11,7 @@ const Columns = styled.div`
 const SignupPage = props => (
   <Columns>
     <Signup />
-    <Signup />
-    <Signup />
+    <Signin />
   </Columns>
 )
 
