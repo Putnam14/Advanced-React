e2b08929967650b3192334e14903254c9d09011e
Putnam14 <putnambr76@gmail.com>
2018-11-10 13:38:53 -0700

    Stop logging password reset requests

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -108,7 +108,6 @@ const Mutations = {
       where: { email: args.email },
       data: { resetToken: resetToken, resetTokenExpiry }
     });
-    console.log(res);
     return { message: "Thanks!" };
     // Email the token
   },
