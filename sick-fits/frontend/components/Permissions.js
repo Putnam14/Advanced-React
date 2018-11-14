53400164ae46eb2134bd2d22c186bbc20e57557f
Putnam14 <putnambr76@gmail.com>
2018-11-13 21:26:03 -0700

    Visual portion of the permission update component done

---
+++ sick-fits/frontend/components/Permissions.js
@@ -0,0 +1,80 @@
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+import React from 'react'
+import Error from './ErrorMessage'
+import Table from './styles/Table'
+import SickButton from './styles/SickButton'
+
+const possiblePermissions = [
+  'ADMIN',
+  'USER',
+  'ITEMCREATE',
+  'ITEMUPDATE',
+  'ITEMDELETE',
+  'PERMISSIONUPDATE',
+]
+
+const ALL_USERS_QUERY = gql`
+  query {
+    users {
+      id
+      name
+      email
+      permissions
+    }
+  }
+`
+
+const Permissions = props => (
+  <Query query={ALL_USERS_QUERY}>
+    {({ data, loading, error }) => (
+      <>
+        <Error error={error} />
+        <>
+          <h2>Manage Permissions</h2>
+          <Table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                {possiblePermissions.map(perm => (
+                  <th>{perm}</th>
+                ))}
+                <th>👇</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.users.map(user => (
+                <User user={user} />
+              ))}
+            </tbody>
+          </Table>
+        </>
+      </>
+    )}
+  </Query>
+)
+
+class User extends React.Component {
+  render() {
+    const user = this.props.user
+    return (
+      <tr>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        {possiblePermissions.map(perm => (
+          <td>
+            <label htmlFor={`${user.id}-permission-${perm}-`}>
+              <input type="checkbox" />
+            </label>
+          </td>
+        ))}
+        <td>
+          <SickButton>Update</SickButton>
+        </td>
+      </tr>
+    )
+  }
+}
+
+export default Permissions
