82766a11d464da9571401c095501909184b837b6
Putnam14 <putnambr76@gmail.com>
2018-11-15 21:50:58 -0700

    Save cart state to local storage, handle toggling cartOpen state

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -1,21 +1,45 @@
 import React from 'react'
+import { Query, Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
 import CartStyles from './styles/CartStyles'
 import Supreme from './styles/Supreme'
 import CloseButton from './styles/CloseButton'
 import SickButton from './styles/SickButton'
 
+const LOCAL_STATE_QUERY = gql`
+  query {
+    cartOpen @client
+  }
+`
+
+const TOGGLE_CART_MUTATION = gql`
+  mutation {
+    toggleCart @client
+  }
+`
+
 const Cart = () => (
-  <CartStyles open>
-    <header>
-      <CloseButton title="close">&times;</CloseButton>
-      <Supreme>Your Cart</Supreme>
-      <p>You have __ items in your cart</p>
-      <footer>
-        <p>$10.10</p>
-        <SickButton>Checkout</SickButton>
-      </footer>
-    </header>
-  </CartStyles>
+  <Mutation mutation={TOGGLE_CART_MUTATION}>
+    {toggleCart => (
+      <Query query={LOCAL_STATE_QUERY}>
+        {({ data }) => (
+          <CartStyles onClick={toggleCart} open={data.cartOpen}>
+            <header>
+              <CloseButton title="close">&times;</CloseButton>
+              <Supreme>Your Cart</Supreme>
+              <p>You have __ items in your cart</p>
+              <footer>
+                <p>$10.10</p>
+                <SickButton>Checkout</SickButton>
+              </footer>
+            </header>
+          </CartStyles>
+        )}
+      </Query>
+    )}
+  </Mutation>
 )
 
 export default Cart
+export { LOCAL_STATE_QUERY }
+export { TOGGLE_CART_MUTATION }
