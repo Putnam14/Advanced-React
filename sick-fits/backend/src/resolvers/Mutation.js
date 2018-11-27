e4f85f45b296396e0e4232f41d692f6a68045e2a
Putnam14 <putnambr76@gmail.com>
2018-11-26 21:05:04 -0700

    Add addToCart mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -203,6 +203,39 @@ const Mutations = {
       },
       info
     );
+  },
+  async addToCart(parents, args, ctx, info) {
+    //Make sure user is signed in
+    const { userId } = ctx.request;
+    if (!userId) throw new Error("You must be signed in");
+    //Query current cart state
+    const [existingCartItem] = await ctx.db.query.cartItems({
+      //cartItems since we want to search by userId
+      where: {
+        user: { id: userId },
+        item: { id: args.id }
+      }
+    });
+    //Check if item is already in cart (add or increment)
+    if (existingCartItem) {
+      console.log("This item is already in their cart");
+      return ctx.db.mutation.updateCartItem(
+        {
+          where: { id: existingCartItem.id },
+          data: { quantity: existingCartItem.quantity + 1 }
+        },
+        info
+      );
+    }
+    return ctx.db.mutation.createCartItem(
+      {
+        data: {
+          user: { connect: { id: userId } },
+          item: { connect: { id: args.id } }
+        }
+      },
+      info
+    );
   }
 };
 
