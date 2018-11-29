0f06be9f70a90ff4e6a53e28635c3bc5cd9dd576
Putnam14 <putnambr76@gmail.com>
2018-11-28 22:16:34 -0700

    Validate id in PropTypes

--- sick-fits/frontend/components/AddToCart.js
+++ sick-fits/frontend/components/AddToCart.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
+import PropTypes from 'prop-types'
 import { CURRENT_USER_QUERY } from './User'
 
 const ADD_TO_CART_MUTATION = gql`
@@ -13,6 +14,10 @@ const ADD_TO_CART_MUTATION = gql`
 `
 
 class AddToCart extends React.Component {
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+  }
+
   render() {
     const { id } = this.props
     return (
