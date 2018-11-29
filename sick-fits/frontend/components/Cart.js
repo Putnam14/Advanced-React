decf4b368015f9fdc361b63e366d4ef6c7c7ed6a
Putnam14 <putnambr76@gmail.com>
2018-11-28 19:52:40 -0700

    Calculate total price of cart

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -7,6 +7,8 @@ import CloseButton from './styles/CloseButton'
 import SickButton from './styles/SickButton'
 import User from './User'
 import CartItem from './CartItem'
+import calcTotalPrice from '../lib/calcTotalPrice'
+import formatMoney from '../lib/formatMoney'
 
 const LOCAL_STATE_QUERY = gql`
   query {
@@ -48,7 +50,7 @@ const Cart = () => (
                     ))}
                   </ul>
                   <footer>
-                    <p>$10.10</p>
+                    <p>{formatMoney(calcTotalPrice(me.cart))}</p>
                     <SickButton>Checkout</SickButton>
                   </footer>
                 </CartStyles>
