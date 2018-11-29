5e6a6aee1d61c602677a4342b145e5ba49928574
Putnam14 <putnambr76@gmail.com>
2018-11-28 19:52:52 -0700

    Style the cart item

--- sick-fits/frontend/components/CartItem.js
+++ sick-fits/frontend/components/CartItem.js
@@ -3,7 +3,20 @@ import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import formatMoney from '../lib/formatMoney'
 
-const CartItemStyles = styled.li``
+const CartItemStyles = styled.li`
+  padding: 1rem 0;
+  border-bottom: 1px solid ${props => props.theme.lightgrey};
+  display: grid;
+  align-items: center;
+  grid-template-columns: auto 1fr;
+  img {
+    margin-right: 10px;
+  }
+  h3,
+  p {
+    margin: 0;
+  }
+`
 
 const CartItem = ({ cartItem }) => (
   <CartItemStyles>
@@ -12,9 +25,9 @@ const CartItem = ({ cartItem }) => (
       <h3>{cartItem.item.title}</h3>
       <p>
         {formatMoney(cartItem.item.price * cartItem.quantity)}
-        {'-'}
+        {' - '}
         <em>
-          {cartItem.quantity} &times; {formatMoney(cartItem.item.price)}
+          {cartItem.quantity} &times; {formatMoney(cartItem.item.price)} each
         </em>
       </p>
     </div>
