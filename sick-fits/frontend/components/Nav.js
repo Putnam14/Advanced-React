d29ee1f71524f1341df5173f8c0e6846ef45cbf4
Putnam14 <putnambr76@gmail.com>
2018-12-01 21:39:11 -0700

    Add the cart count to the nav

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -4,6 +4,7 @@ import NavStyles from './styles/NavStyles'
 import User from './User'
 import Signout from './Signout'
 import { TOGGLE_CART_MUTATION } from './Cart'
+import CartCount from './CartCount'
 
 const Nav = () => (
   <User>
@@ -29,6 +30,12 @@ const Nav = () => (
               {toggleCart => (
                 <button type="button" onClick={toggleCart}>
                   My Cart
+                  <CartCount
+                    count={me.cart.reduce(
+                      (tally, cartItem) => tally + cartItem.quantity,
+                      0
+                    )}
+                  />
                 </button>
               )}
             </Mutation>
