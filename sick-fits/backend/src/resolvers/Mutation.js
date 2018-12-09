891eb7116fa9f95498ab2571b8bd1653ec330bbf
Putnam14 <putnambr76@gmail.com>
2018-12-08 23:45:43 -0700

    Create orderItems and delete cartItems

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -301,10 +301,31 @@ const Mutations = {
       source: args.token
     });
     // Convert the cart items to order items
+    const orderItems = user.cart.map(cartItem => {
+      const orderItem = {
+        ...cartItem.item,
+        quantity: cartItem.quantity,
+        user: { connect: { id: userId } }
+      };
+      delete orderItem.id;
+      return orderItem;
+    });
     // Create the order
+    const order = await ctx.db.mutation.createOrder({
+      data: {
+        total: charge.amount,
+        charge: charge.id,
+        items: { create: orderItems },
+        user: { connect: { id: userId } }
+      }
+    });
     // Clear the user's cart
-    // Delete cart items
+    const cartItemIds = user.cart.map(cartItem => cartItem.id);
+    await ctx.db.mutation.deleteManyCartItems({
+      where: { id_in: cartItemIds }
+    });
     // Return the order to the client
+    return order;
   }
 };
 
