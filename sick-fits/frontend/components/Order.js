362fc571a6ee38aa1e58d40e905d57a9594c4b1b
Putnam14 <putnambr76@gmail.com>
2018-12-09 21:04:11 -0700

    Order component for order page

---
+++ sick-fits/frontend/components/Order.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Query } from 'react-apollo'
+import { format } from 'date-fns'
+import Head from 'next/head'
+import gql from 'graphql-tag'
+import formatMoney from '../lib/formatMoney'
+import Error from './ErrorMessage'
+import OrderStyles from './styles/OrderStyles'
+
+const SINGLE_ORDER_QUERY = gql`
+  query SINGLE_ORDER_QUERY($id: ID!) {
+    order(id: $id) {
+      id
+      charge
+      total
+      createdAt
+      user {
+        id
+      }
+      items {
+        id
+        title
+        description
+        price
+        image
+        quantity
+      }
+    }
+  }
+`
+
+class Order extends React.Component {
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+  }
+
+  render() {
+    const { id } = this.props
+    return (
+      <Query query={SINGLE_ORDER_QUERY} variables={{ id }}>
+        {({ data, error, loading }) => {
+          const { order } = data
+          if (error) return <Error error={error} />
+          if (loading) return <p>Loading...</p>
+          return (
+            <OrderStyles>
+              <Head>
+                <title>Sick Fits - Order {order.id}</title>
+              </Head>
+              <p>
+                <span>Order ID:</span>
+                <span>{order.id}</span>
+              </p>
+              <p>
+                <span>Charge:</span>
+                <span>{order.charge}</span>
+              </p>
+              <p>
+                <span>Created:</span>
+                <span>{format(order.createdAt, 'MMMM d, yyyy h:mm a')}</span>
+              </p>
+              <p>
+                <span>Total:</span>
+                <span>{formatMoney(order.total)}</span>
+              </p>
+              <p>
+                <span>Item Count:</span>
+                <span>{order.items.length}</span>
+              </p>
+              <div className="items">
+                {order.items.map(item => (
+                  <div className="order-item" key={item.id}>
+                    <img src={item.image} alt={item.title} />
+                    <div className="item-details">
+                      <h2>{item.title}</h2>
+                      <p>Qty: {item.quantity}</p>
+                      <p>Each: {formatMoney(item.price)}</p>
+                      <p>Subtotal: {formatMoney(item.price * item.quantity)}</p>
+                      <p>{item.description}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </OrderStyles>
+          )
+        }}
+      </Query>
+    )
+  }
+}
+
+export default Order
