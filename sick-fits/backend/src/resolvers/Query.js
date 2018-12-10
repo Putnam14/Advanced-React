c696545dd1af8ceeb33bd4d87cc7da2bc114a8c4
Putnam14 <putnambr76@gmail.com>
2018-12-09 21:04:00 -0700

    Add a single order query

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -24,6 +24,21 @@ const Query = {
 
     // If they do, query all the users
     return ctx.db.query.users({}, info); // info includes the graphql query coming from the client
+  },
+  async order(parent, args, ctx, info) {
+    // 1. Make sure they are logged in
+    if (!ctx.request.userId) throw new Error("You must be logged in!");
+    // 2. Query the current order
+    const order = await ctx.db.query.order({ where: { id: args.id } }, info);
+    // 3. Check if they have the permissions to see this order
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes(
+      "ADMIN"
+    );
+    if (!ownsOrder || !hasPermissionToSeeOrder)
+      throw new Error("You can't see this.");
+    // 4. Return the order
+    return order;
   }
 };
 
