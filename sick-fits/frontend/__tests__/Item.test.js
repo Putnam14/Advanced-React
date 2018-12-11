d7756e34ece39dfbb9bea2b9d99786d61a5a96db
Putnam14 <putnambr76@gmail.com>
2018-12-10 20:50:43 -0700

    Test shallow rendering components using enzyme

---
+++ sick-fits/frontend/__tests__/Item.test.js
@@ -0,0 +1,42 @@
+import { shallow } from 'enzyme'
+import ItemComponent from '../components/Item'
+
+const fakeItem = {
+  id: 'ABC123',
+  title: 'A cool item',
+  price: 5000,
+  description: 'This item is really cool!',
+  image: 'dog.jpeg',
+  largeImage: 'largedog.jpeg',
+}
+
+describe('<Item />', () => {
+  it('renders the image', () => {
+    // Shallow render the Item component
+    const wrapper = shallow(<ItemComponent item={fakeItem} />)
+    const Image = wrapper.find('img')
+    expect(Image.props().src).toBe(fakeItem.image)
+    expect(Image.props().alt).toBe(fakeItem.title)
+  })
+
+  it('renders the title and price tag', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />)
+    const PriceTag = wrapper.find('PriceTag')
+    expect(PriceTag.children().text()).toBe('$50')
+    expect(wrapper.find('Title a').text()).toBe(fakeItem.title)
+  })
+
+  it('renders out the buttons', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />)
+    const buttonList = wrapper.find('.buttonList')
+    expect(buttonList.children()).toHaveLength(3)
+    // Three ways to see if a tag exists:
+    expect(buttonList.find('Link')).toHaveLength(1)
+    expect(buttonList.find('Link').exists()).toBe(true)
+    expect(buttonList.find('Link')).toBeTruthy()
+
+    expect(buttonList.find('AddToCart').exists()).toBe(true)
+    expect(buttonList.find('DeleteItem').exists()).toBe(true)
+    console.log(buttonList.debug())
+  })
+})
