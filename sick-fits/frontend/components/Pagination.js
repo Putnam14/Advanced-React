4e8f40bf9916df6e492ae29899b036770cec97aa
Putnam14 <putnambr76@gmail.com>
2018-12-12 20:19:05 -0700

    Fix className for next button, make the component testable

--- sick-fits/frontend/components/Pagination.js
+++ sick-fits/frontend/components/Pagination.js
@@ -24,7 +24,7 @@ const Pagination = props => (
       const pages = Math.ceil(count / perPage) || 1
       const { page } = props
       return (
-        <PaginationStyles>
+        <PaginationStyles data-test="pagination">
           <Head>
             <title>
               Sick Fits! - Page {page} of {pages}
@@ -42,7 +42,7 @@ const Pagination = props => (
             </a>
           </Link>
           <p>
-            Page {page} of {pages}
+            Page {page} of <span className="totalPages">{pages}</span>
           </p>
           <p>{count} Items Total</p>
           <Link
@@ -52,7 +52,7 @@ const Pagination = props => (
               query: { page: page + 1 },
             }}
           >
-            <a className="prev" aria-disabled={page >= pages}>
+            <a className="next" aria-disabled={page >= pages}>
               Next ⏭️
             </a>
           </Link>
@@ -63,3 +63,4 @@ const Pagination = props => (
 )
 
 export default Pagination
+export { PAGINATION_QUERY }
