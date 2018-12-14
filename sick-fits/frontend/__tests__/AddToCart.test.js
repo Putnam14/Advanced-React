4a5223a4d35e805af74719f0ad1f3ae69074e33c
Putnam14 <putnambr76@gmail.com>
2018-12-13 20:37:52 -0700

    Test the <AddToCart /> button

---
+++ sick-fits/frontend/__tests__/AddToCart.test.js
@@ -0,0 +1,88 @@
+import { mount } from 'enzyme'
+import toJSON from 'enzyme-to-json'
+import wait from 'waait'
+import { MockedProvider } from 'react-apollo/test-utils'
+import { ApolloConsumer } from 'react-apollo'
+import { fakeUser, fakeCartItem } from '../lib/testUtils'
+import Cart, { LOCAL_STATE_QUERY } from '../components/Cart'
+import { CURRENT_USER_QUERY } from '../components/User'
+import AddToCart, { ADD_TO_CART_MUTATION } from '../components/AddToCart'
+
+const mocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: {
+      data: {
+        me: {
+          ...fakeUser(),
+          cart: [],
+        },
+      },
+    },
+  },
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: {
+      data: {
+        me: {
+          ...fakeUser(),
+          cart: [fakeCartItem()],
+        },
+      },
+    },
+  },
+  {
+    request: { query: ADD_TO_CART_MUTATION, variables: { id: 'abc123' } },
+    result: {
+      data: {
+        addToCart: {
+          ...fakeCartItem(),
+          quantity: 1,
+        },
+      },
+    },
+  },
+]
+
+describe('<AddToCart/>', () => {
+  it('renders and matches the snapshot', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <AddToCart id="abc123" />
+      </MockedProvider>
+    )
+    await wait()
+    wrapper.update()
+    expect(toJSON(wrapper.find('button'))).toMatchSnapshot()
+  })
+  it('Adds an item to cart when clicked', async () => {
+    let apolloClient
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <ApolloConsumer>
+          {client => {
+            apolloClient = client
+            return <AddToCart id="abc123" />
+          }}
+        </ApolloConsumer>
+      </MockedProvider>
+    )
+    await wait()
+    wrapper.update()
+    const {
+      data: { me },
+    } = await apolloClient.query({ query: CURRENT_USER_QUERY })
+    expect(me.cart).toHaveLength(0)
+    // Add an item to the cart
+    wrapper.find('button').simulate('click')
+    await wait(50)
+    wrapper.update()
+    // Check if the item is in the cart
+    const {
+      data: { me: me2 },
+    } = await apolloClient.query({ query: CURRENT_USER_QUERY })
+    expect(me2.cart).toHaveLength(1)
+    expect(me2.cart[0].id).toBe('omg123')
+    expect(me2.cart[0].quantity).toBe(3)
+  })
+})
